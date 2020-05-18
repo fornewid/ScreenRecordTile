7e3d46aa737508177a2f2cf67d9ea70b2106b663
SOUP <soupyong@gmail.com>
2020-05-19 01:43:47 +0900

    App: Upgrade to the Firebase Crashlytics SDK
    https://firebase.google.com/docs/crashlytics/upgrade-sdk?platform=android

--- app/src/main/java/soup/tile/screenrecord/ScreenRecordApplication.kt
+++ app/src/main/java/soup/tile/screenrecord/ScreenRecordApplication.kt
@@ -2,12 +2,18 @@ package soup.tile.screenrecord
 
 import android.app.Application
 import soup.tile.screenrecord.notification.NotificationInfo
+import soup.tile.screenrecord.util.ReleaseTree
+import timber.log.Timber
 
 class ScreenRecordApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        BuildType.init(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(ReleaseTree())
+        }
         NotificationInfo.createChannels(this)
     }
 }
